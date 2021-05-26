e96fe7a8dba8c89facf9235299c65b50b5dc46cd
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-05-26 10:50:06 +0300

    correcting mistake

--- app/src/main/java/com/example/poplibraries_hw/ui/MainActivity.kt
+++ app/src/main/java/com/example/poplibraries_hw/ui/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.poplibraries_hw.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.poplibraries_hw.databinding.ActivityMainBinding
 import com.example.poplibraries_hw.mvp.presenter.MainPresenter
 import com.example.poplibraries_hw.mvp.view.MainView
@@ -15,20 +14,21 @@ class MainActivity : AppCompatActivity(), MainView {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
-        }
-        binding.btnCounter1.setOnClickListener(listener)
-        binding.btnCounter2.setOnClickListener(listener)
-        binding.btnCounter3.setOnClickListener(listener)
+
+        binding.btnCounter1.setOnClickListener { presenter.counter1Click() }
+        binding.btnCounter2.setOnClickListener { presenter.counter2Click() }
+        binding.btnCounter3.setOnClickListener { presenter.counter3Click() }
     }
 
-    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
-    override fun setButtonText(index: Int, text: String) {
-        when (index) {
-            0 -> binding.btnCounter1.text = text
-            1 -> binding.btnCounter2.text = text
-            2 -> binding.btnCounter3.text = text
-        }
+    override fun setButton1Text(text: String) {
+        binding.btnCounter1.text = text
+    }
+
+    override fun setButton2Text(text: String) {
+        binding.btnCounter2.text = text
+    }
+
+    override fun setButton3Text(text: String) {
+        binding.btnCounter3.text = text
     }
 }
